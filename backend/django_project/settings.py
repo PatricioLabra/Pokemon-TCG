8a1399c3e44d4863552a88ec06328f3d176a2b24
Patricio Labra <patricio.labra827@gmail.com>
2025-01-12 21:07:29 -0300

    UP: Se actualiza la generacion de la metadata y se ajusta responsividad del pop-up

--- backend/django_project/settings.py
+++ backend/django_project/settings.py
@@ -138,7 +138,7 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ALLOWED_ORIGINS = [
-    "http://localhost:3001", "http://localhost:3000"
+    "http://localhost:3001", "http://localhost:3000", "http://localhost:3002"
 ]
 
 LOGGING = {
